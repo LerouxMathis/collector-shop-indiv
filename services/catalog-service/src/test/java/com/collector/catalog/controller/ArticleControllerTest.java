fa24c658592a28a4680e765ae529d931ed8c8162
LerouxMathis <mathisleroux32@gmail.com>
2026-04-12 18:00:47 +0200

    bug fix Test unitaire

--- services/catalog-service/src/test/java/com/collector/catalog/controller/ArticleControllerTest.java
+++ services/catalog-service/src/test/java/com/collector/catalog/controller/ArticleControllerTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,8 +32,10 @@ class ArticleControllerTest {
     @Autowired
     private ObjectMapper objectMapper; 
 
+    @MockBean
     private ArticleRepository repository;
 
+    @MockBean
     private KafkaProducerService kafkaService;
 
     @Test
