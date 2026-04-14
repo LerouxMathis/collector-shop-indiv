07454f527d1d9b149454aba997c92d2d17f723ce
LerouxMathis <mathisleroux32@gmail.com>
2026-04-14 22:15:10 +0200

    monitoring

--- scripts/load-testing/run-siege-test.sh
+++ scripts/load-testing/run-siege-test.sh
@@ -2,5 +2,5 @@
 set -e
 echo "2000 requêtes totales, 100 utilisateurs simultanés"
 
-TARGET_URL="http://host.docker.internal:8081/api/articles"
+TARGET_URL="http://host.docker.internal/api/articles"
 docker run --rm alpine sh -c "apk add --no-cache apache2-utils > /dev/null && ab -n 2000 -c 100 ${TARGET_URL}/"
